db0f15254ad51b4dae2545ba2b29c1aa28fc4f62
Pratik <planetpratik@live.in>
2018-04-18 16:38:50 +0530

    docs: Add comments to the code
    Increase readability of the code

--- src/components/HomePageTitle.js
+++ src/components/HomePageTitle.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+
+// Stateless functional component is used as we are using only render method.
+// Title is immutable property, hence it's passed as a prop rather than using state.
 const HomePageTitle = (props) => {
     return (
-        <div className="list-books-title">
+        <div className='list-books-title'>
             <h1>{props.title}</h1>
         </div>
     )
 }
+
+// Default prop value is set using .defaultProps property
 HomePageTitle.defaultProps = {
-    title: "MyReads"
+    title: 'MyReads'
 }
+
+// PropTypes are used to run typechecking on the props for a component
 HomePageTitle.PropTypes = {
     title: PropTypes.string.isRequired
 }
