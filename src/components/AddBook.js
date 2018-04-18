76e00549fc6e4d2d8a97b5629b4d4aed5ec75d64
Pratik <planetpratik@live.in>
2018-04-18 16:58:23 +0530

    feat: Implement add new book link with navigation
    Link component from react-router-dom provides declarative, accessible navigation
    around the application

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
+// Stateless functional component is used as we are using only render method.
 const AddBook = () => {
     return (
-        <div>
-
+        <div className='open-search'>
+               <Link to='/search'>Add New Book</Link>
         </div>
     )
 }
